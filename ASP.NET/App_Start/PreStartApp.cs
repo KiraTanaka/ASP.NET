9d2f05665f7114d9dffd9f02c0fbbb40eb1019ca
Kira Tanaka <suein_i@mail.ru>
2015-10-04 13:05:28 +0500

    Add project

---
+++ ASP.NET/App_Start/PreStartApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebActivatorEx;
+using ASP;
+
+//[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PreStartApp), "Start")]
+namespace ASP.NET.App_Start
+{  
+    public class PreStartApp
+    {      
+            private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+            /// <summary>
+            /// Метод запускается один раз перед стартом приложения        
+            /// </summary>
+            public static void Start()
+            {
+                logger.Info("Application PreStart");
+            }
+    }
+}
